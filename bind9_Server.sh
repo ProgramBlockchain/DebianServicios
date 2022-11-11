45b23b6aff1bf50a8f0b9cd10a9ef4f1a3db5842
Pabloxd <andreceva123@gmail.com>
2022-11-10 22:19:13 -0500

    Se pide la direccion ip del usuario y se crea el archivo con la direccion del local host

--- bind9_Server.sh
+++ bind9_Server.sh
@@ -2,13 +2,17 @@
 #
 # Programa para instalar servicio de DNS con bind9
 # Nombre: Cevallos Pablo
+read -p "Ingresa tu direccion IPv4:" my_ip
 # Actualizar paquetes
 sudo apt update && sudo apt upgrade
-
 # Intalacion del servicio vim
 sudo apt install -y bind9 dnsutils
 # Redireccionar la salida a /etc/bind/named.conf.options cambiando los forwarder
 # a 8.8.8.8 
 cat named.conf.options_cp > /etc/bind/named.conf.options
 
+cat prueba.sh > /etc/resolv.conf
 
+echo "Tu direccion ip es: $my_ip "
+
+systemctl restart bind9
